7fa97a6d1cc1ed85a3756fe4dd9b745f3e077086
Muhammad Abdullah - (SQA - SS Team) <m.abdullah@csd.local>
2018-10-22 23:29:09 +0500

    Hello 3rd

---
+++ hello3.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class hello3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("USA is the land of opportunity");
+		
+	}
+
+}
